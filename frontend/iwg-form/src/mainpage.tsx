944b1f8968dd4e9e3a9d813249cb920c790b6d54
khanabdu25 <khanabdu@umich.edu>
2023-12-21 02:22:59 -0500

    Added resizing for mobile
    - added resizing for smaller screens (mobile)
    - added logo in top right of form area

--- frontend/iwg-form/src/mainpage.tsx
+++ frontend/iwg-form/src/mainpage.tsx
@@ -1,15 +1,40 @@
 import React from 'react';
-import './mainpage.css'; // Create a separate CSS file for MainPage styles if needed
+import './mainpage.css';
+import './formcomponents.css';
+import imageSrc from './assets/tmc-logo.jpeg';
 
 const MainPage: React.FC = () => {
-  return (
-    <main id="mainpage">
-    <div className="form-container">
-      <h1 className="form-heading">Report an Incident</h1>
-      {/* The rest of your form will go here */}
-    </div>
-  </main>
-  );
+    return (
+        <main id="mainpage">
+            <div className="form-container">
+                <div className="parent">
+                    {/* header div */}
+                    <div className="child">
+                        <h1 className="form-heading">Report an Incident</h1>
+                    </div>
+                    {/* image div */}
+                    <div className="child">
+                        <img
+                            style={{
+                                maxWidth: '60px',
+                                maxHeight: '61px',
+                                height: 'auto',
+                                width: 'auto',
+                                flexShrink: 0
+                            }}
+                            src={imageSrc}
+                            alt="Description"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <span className="asterisk">*</span>
+                    <span className="information"> </span>
+                    <span className="required">Required</span>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default MainPage;
